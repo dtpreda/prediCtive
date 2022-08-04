4e3ce321901f7371a64526b805d0348cfdcbdc3f
David Preda <dtpreda30@gmail.com>
2022-08-04 21:38:54 +0100

    Update grammar test

--- test/grammar_test.cpp
+++ test/grammar_test.cpp
@@ -20,23 +20,4 @@ TEST_F(prediCtiveParserTest, SimpleGrammar) {
     std::string contents = TestUtils::openPrediCtiveFile("simpleGrammar.cg");
 
     ASSERT_NO_THROW(prediCtiveParser.parse(contents));
-
-    Node root = prediCtiveParser.parse(contents);
-    ASSERT_EQ(root.getName(), "SStart");
-
-    Node start = root.getChild(0);
-    Node eof = root.getChild(1);
-    ASSERT_EQ(start.getName(), "Start");
-    ASSERT_EQ(eof.getName(), "EOF");
-
-    Node tokens = start.getChild(0);
-    ASSERT_EQ(tokens.getName(), "Tokens");
-
-    ASSERT_EQ(tokens.getChild(0).getName(), "Tokens");
-    ASSERT_EQ(tokens.getChild(1).getName(), "Left Curly Bracket");
-    Node token = tokens.getChild(2);
-    ASSERT_EQ(token.getName(), "Token");
-    Node nextToken = tokens.getChild(3);
-    ASSERT_EQ(nextToken.getName(), "NextToken");
-    ASSERT_EQ(tokens.getChild(4).getName(), "Right Curly Bracket");
 }
