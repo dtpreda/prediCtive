9e1f7e9135dcfd0c43a26b66b41f9ef8154b2b4d
David Preda <dtpreda30@gmail.com>
2022-08-30 17:32:22 +0100

    Add test for SemanticCheckVisitor

--- test/grammar_test.cpp
+++ test/grammar_test.cpp
@@ -11,6 +11,7 @@
 #include "parser/visitors/astConversion/RuleExtractorVisitor.h"
 #include "parser/visitors/astConversion/RuleSimplifierVisitor.h"
 #include "parser/visitors/astConversion/ClosureSimplifierVisitor.h"
+#include "parser/visitors/semantic/SemanticCheckVisitor.h"
 
 class prediCtiveParserTest : public ::testing::Test {
 protected:
@@ -348,4 +349,18 @@ TEST_F(prediCtiveParserTest, BundleVisiting) {
     ASSERT_EQ("Intermediate_NonTerminal_1", emptyRule2->getAnnotation("name"));
 
     ASSERT_EQ(0, emptyRule2->getChildren().size());
+}
+
+TEST_F(prediCtiveParserTest, SemanticCheck) {
+    std::string contents = TestUtils::openPrediCtiveFile("simpleGrammar.cg");
+
+    ASSERT_NO_THROW(prediCtiveParser.parse(contents));
+
+    std::shared_ptr<Node> root = prediCtiveParser.parse(contents);
+
+    convertToAST(root);
+
+    SemanticCheckVisitor scv;
+
+    ASSERT_NO_THROW(scv.visit(root));
 }
