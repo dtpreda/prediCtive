e5a128563d454e28a67a8738f74365af660ebfe9
David Preda <dtpreda30@gmail.com>
2023-01-17 17:09:02 +0000

    Add parse-content endpoint

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -20,5 +20,18 @@ int main() {
         return crow::response(crow::status::OK);
     });
 
+    CROW_ROUTE(app, "/parse-content")
+    .methods("POST"_method)
+    ([](const crow::request& req){
+        auto json = crow::json::load(req.body);
+        if (!json)
+            return crow::response(crow::status::BAD_REQUEST); // same as crow::response(400)
+        if (!json.has("content")) {
+            crow::json::wvalue response({{"error", "Missing \'content\' field."}});
+            return crow::response(crow::status::BAD_REQUEST, response);
+        }
+        return crow::response(crow::status::OK);
+    });
+
     app.port(18080).multithreaded().run();
 }
