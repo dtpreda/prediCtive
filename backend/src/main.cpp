e344e3b14cca09c376e9d133da6e1498207eb16a
David Preda <dtpreda30@gmail.com>
2023-01-17 16:45:50 +0000

    Setup crow

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,38 +1,12 @@
 #include <iostream>
+#include <crow.h>
 
-#include "parser/prediCtiveParser.h"
-#include "parser/visitors/grammar/GrammarBuilderVisitor.h"
-#include "parser/grammar/GrammarBuilder.h"
-#include "parser/visitors/astConversion/ParseTreeSimplifierVisitor.h"
+int main() {
+    crow::SimpleApp app;
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cout << "Wrong number of arguments" << std::endl;
-        return -1;
-    }
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
 
-    Parser prediCtiveParser = buildPrediCtiveParser();
-
-    std::shared_ptr<Node> root = prediCtiveParser.openAndParse(argv[1]);
-
-    convertToAST(root);
-
-    GrammarBuilderVisitor gbv;
-
-    gbv.visit(root);
-
-    GrammarBuilder gv = gbv.grammarBuilder;
-    gv.computeSets();
-
-    Parser newGrammarParser = gv.buildGrammar();
-
-    std::shared_ptr<Node> parseTree = newGrammarParser.openAndParse(argv[2]);
-
-    ParseTreeSimplifierVisitor ptsv;
-
-    ptsv.visit(parseTree);
-
-    std::cout << parseTree->print() << std::endl;
-
-    return 0;
+    app.port(18080).multithreaded().run();
 }
