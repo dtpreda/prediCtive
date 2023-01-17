9d23bc0262ea8cf6ce68214f7f4aac0b578aafe7
David Preda <dtpreda30@gmail.com>
2023-01-17 17:07:00 +0000

    Add grammar endpoint

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <crow.h>
 
 int main() {
@@ -6,6 +5,19 @@ int main() {
 
     CROW_ROUTE(app, "/")([](){
         return "Hello world";
+    });
+
+    CROW_ROUTE(app, "/grammar")
+    .methods("POST"_method)
+    ([](const crow::request& req){
+        auto json = crow::json::load(req.body);
+        if (!json)
+            return crow::response(crow::status::BAD_REQUEST); // same as crow::response(400)
+        if (!json.has("grammar")) {
+            crow::json::wvalue response({{"error", "Missing \'grammar\' field."}});
+            return crow::response(crow::status::BAD_REQUEST, response);
+        }
+        return crow::response(crow::status::OK);
     });
 
     app.port(18080).multithreaded().run();
